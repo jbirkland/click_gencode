f2febf84ea1c650954b4df062d29ca0f97ba5716
jbirkland <jbirk05@gmail.com>
2021-09-19 01:08:40 -0500

    phase1,fuctions complete.Readme partial

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # click_gencode
 # 03 JavaScript: Password Generator
+
+## Ojective
+Create a function in javascript that generates a random code for security purposes for a client. This function must present the user with a series of prompts for password criteria. When all prompts are answered it generates a password with all of the information that was gathered and displays to the user. The file being created must follow best coding format practices along with not errors while loading the web page or dev console.
+
+### Screen Shot
+Here is the screenshot file path to my webpage.
+<!-- //screen shot needed !!!!!!!!!!!!!!!!! -->
+
+
+
+## Link to the Deployed Page
+
+Here is a link to my delpoyed code. https://jbirkland.github.io/click_gencode/
--- script.js
+++ script.js
@@ -1,39 +1,97 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");// Do not touch!!!
 
+var numberOfCharacters = 0
+var specialCharacters = true;[!"#$%&'()*+,-./:;<=>?@[]^_`{|}~"]
+var numbersAllowed = true;[123456789]
+var lowercaseAllowed = true;["abcdefghijklmnopqrstuvwxyz"]
+var uppercaseAllowed = true;["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+// __________________________________________________________________________
+function askHowManyCharacters(){
+numberOfCharacters = window.prompt("How many characters do you want in the password?");
+var answer1 = (numberOfCharacters);
+console.log (answer1);
+}
+// if ( characters > 15 ){
+//   var symbols = window.prompt("Do you want to use symbols?");
+//     console.log(characters)
+//   }
+//   else if (characters >= 25 ){
+//     var characters = window.confirm ("Password must be between 8-15 characters");
+//   }
+
+function askSpecialCharactersAllowed(){
+specialCharacters = window.prompt("Would you like special characters in the password?");
+var answer2 = (specialCharacters);  
+console.log (answer2)
+}
+
+function askNumbersAllowed(){
+  numbersAllowed = window.prompt("Would you like numbers in the password?"); 
+  var answer3 = (numbersAllowed);
+  console.log (answer3)
+}
+
+function askLowerCase(){
+  lowercaseAllowed = window.prompt("Would you like lower case letters in the password?"); 
+  var answer4 = (lowercaseAllowed)
+  console.log (answer4)
+}
+
+function askUpperCase(){
+  uppercaseAllowed = window.prompt("Would you like upper case letters in the password?"); 
+  var answer5 = (uppercaseAllowed)
+  console.log (answer5)
+}
+
+
+
+askHowManyCharacters();
+askSpecialCharactersAllowed();
+askNumbersAllowed();
+askLowerCase();
+askUpperCase();
+// _____________________________________________________________________________
+function generatePassword(){
+  var finalPassword ="";
+  var numberOfCharacters = askHowManyCharacters()
+
+  return finalPassword
+}
 
 //Pseudocoding
-//1.) Need to know how many charachter athe password will have
+//1.) Need to know how many charachters are in the password will have
 //2.) Need to know are special characters included
-// Need to know are numbers included
+// Need to know are numbers included                        {} WRITE FUNCTIONS FOR THESE
 // Need to know are lower case letters included
 // Need to know are upper case letters included
 
-// Build final list of possible characters
+// Build final list of possible characters        {} CERTAIN DATA TYPE YOU CAN USE TO BUILD THIS
+
 // How can we build our password one letter at a time?
 // We need a way to store all the possible characters allowed in the password
 // For each character needed, grab a random element from the allowed list
 
 
-function askHowManyCharacters(){
+// function askHowManyCharacters(){
 
-}
-
-
-function generatePassword(){
-var finalPassword ="";
-var numberOfCharacters = askHowManyCharacters();
-
-return finalPassword;
-
-}
+// }
 
 
+// function generatePassword(){
+// var finalPassword ="";
+// var numberOfCharacters = askHowManyCharacters();
+
+// return finalPassword;
+
+// }
 
 
 
 
-// Write password to the #password input
+
+
+// Write password to the #password input    //  DO NOT TOUCH!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
